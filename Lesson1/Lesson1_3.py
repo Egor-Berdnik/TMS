23ed6d0540572cd95a53ef498fd64e1a7e0cfee7
Egor-Berdnik <goldenbereg97@gmail.com>
2022-11-22 12:42:18 +0300

    Lesson1

---
+++ Lesson1/Lesson1_3.py
@@ -0,0 +1,3 @@
+x = int(input("x = "))
+y = int(input("y = "))
+print("Выражение равняется ", ((abs(x) - abs(y)) / (1 + abs(x*y))))
---
+++ Lesson1/Slices.py
@@ -0,0 +1,13 @@
+my_string = "aaaaaBccBddBeeBffBggB"
+print("result:", my_string[5::3])
+
+my_string1 = "AAAABBBBCCCCDDDDFFFF"
+print(my_string1[:4])
+print(my_string1[4:8])
+print(my_string1[8:12])
+print(my_string1[12:16])
+print(my_string1[16:20])
+print(my_string1[:4]+my_string1[4:8]+my_string1[8:12]+my_string1[12:16]+my_string1[16:20])
+
+my_string2 = "PYTHON"
+print( my_string2[::-1])
