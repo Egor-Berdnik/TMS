38c88e38ceeb1dd91a20da83a40305a6c42161ee
Egor-Berdnik <goldenbereg97@gmail.com>
2022-12-12 22:06:06 +0300

    Lesson6

---
+++ Lesson6/Lesson6_2.py
@@ -0,0 +1,8 @@
+a, b, c, d = input("Введите 4 строки: 1. "), input("2: "), input("3: "), input("4: ")
+print(a, b, c, d)
+f.write(a + "\n")
+f.write(b + "\n")
+f.close()
+f = open("Homework.txt.", "a")
+f.write(c + "\n")
+f.write(d + "\n")
